be05d769beebec255447446d7146be7c78d5ea3f
KJ Johnson <kjohns95@emich.edu>
2022-09-25 23:00:15 -0400

    working on getting characters to show up - modified character models

--- src/types/character.types.ts
+++ src/types/character.types.ts
@@ -1,10 +1,27 @@
 export interface Character {
-  _id: string;
-  userId: string;
-  characterUsername: string;
+  _id?: string;
+  userId?: string;
   characterName: string;
-  characterDescription: string;
-
-  createdAt: string;
-  updatedAt: string;
+  createdAt?: string;
+  updatedAt?: string;
+  class: string;
+  level: number;
+  race: string;
+  alignment: string;
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+  proficiencyBonus: number;
+  armorClass: number;
+  speed: string;
+  hitPoints: number;
+  spells: string[];
+  proficiencies: string[];
+  languages: string[];
+  equipment: string[];
+  features: string[];
+  traits: string[];
 }
